1e74a0fe38fb6341be0954ee62c1952756c0a942
Andrey Boukreev <ruzoomparty@gmail.com>
2023-12-07 15:36:57 +0300

    Json endpoint findAll

---
+++ voice/src/main/java/com/zoomvoice/voice/controller/StatisticController.java
@@ -0,0 +1,23 @@
+package com.zoomvoice.voice.controller;
+
+import com.zoomvoice.voice.domain.entity.Person;
+import com.zoomvoice.voice.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/statistic")
+public class StatisticController {
+    private final UserRepository userRepository;
+    @GetMapping()
+    public ResponseEntity<List<Person>> findAll() {
+        List<Person> persons = userRepository.findAll();
+        return ResponseEntity.ok(persons);
+    }
+}
